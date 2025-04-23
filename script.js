c284bb8fa07b7fa816cdd3e56a1d69f9e065fee8
JuanD-000 <juanplacencio123@gmail.com>
2025-04-23 01:34:51 -0400

    removed

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("create-lobby").addEventListener("click", function() {
-    alert("Lobby Created! (Multiplayer feature coming soon!)");
-});
-
-document.getElementById("join-lobby").addEventListener("click", function() {
-    let code = document.getElementById("room-code").value;
-    alert("Joining lobby: " + code);
-});
